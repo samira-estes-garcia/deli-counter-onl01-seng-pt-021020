af0cfc7152ecc3f2cbe792178b41a17589b93a4e
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-02-20 00:50:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def line(array)
+  if array.empty?
+    puts "The line is currently empty."
+  else
+    line_arr = "The line is currently: "
+    array.each_with_index{|name, index| line_arr << "#{index + 1}. #{name} "}
+    puts line_arr
+  end
+end
