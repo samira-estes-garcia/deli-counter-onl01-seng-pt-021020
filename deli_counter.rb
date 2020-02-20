7e9124d1bb67e86a83857df0b5d3a5c441c0b291
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-02-20 00:55:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(array)
     puts "The line is currently empty."
   else
     line_arr = "The line is currently: "
-    array.each_with_index{|name, index| line_arr << "#{index + 1}. #{name} "}
+    array.each_with_index{|name, index| line_arr << " #{index + 1}. #{name} "}
     puts line_arr
   end
 end
